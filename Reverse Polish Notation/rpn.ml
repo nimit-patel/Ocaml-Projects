cf34c8c030796f3217c4d851856650a0f4dc9f1d
nimitp <nimit444@gmail.com>
2019-02-14 18:15:12 -0500

    Removed is_float method

--- Reverse Polish Notation/rpn.ml
+++ Reverse Polish Notation/rpn.ml
@@ -5,7 +5,9 @@ type result =                                   (* Sum type either float or stri
 
 let calc stack op : result =
   try
-    Num (op (Stack.pop stack) (Stack.pop stack)) (* Evaluate expression by popping two values from stack*)
+    Num (op (Stack.pop stack) (Stack.pop stack)) (* Evaluate expression by popping two values from stack.
+                                                    The second pop operation is done first so, evaluation 
+                                                    is first <op> second *)
   with
     | Stack.Empty      -> Error "Invalid RPN Expression: not enough operands!" 
     | _                -> Error "Something went wrong!"
@@ -13,9 +15,9 @@ let calc stack op : result =
 
 let get_float_value token stack : result = 
   try 
-    Num (float_of_string(token))
+    Num ( float_of_string(token) )
   with
-    | _                 -> Error ("Error: " ^ token ^ " cannot be converted to a float")
+    | _                 -> Error ("Error: \'" ^ token ^ "\' cannot be converted to a float")
   ;;
 
 let parse_token token stack : result =
@@ -28,17 +30,11 @@ let parse_token token stack : result =
   |  _  -> get_float_value token stack
   ;;
 
-let is_float value : bool= 
-  match value with
-  | Num num -> true
-  | Error error -> false
-  ;;
-
 let rec eval tokens stack : result=
   match tokens with 
-  | []         -> if (Stack.length stack) = 1       (* Exactly one value should be on stack after evaluating rpn *)
+  | []         -> if          (Stack.length stack) = 1    (* Exactly one value should be on stack after evaluating rpn *)
                   then Num    (Stack.pop stack) 
-                  else Error "Invalid RPN expression: not enough operators!"
+                  else Error  "Invalid RPN expression: not enough operators!"
   | curr::rest -> let result = (parse_token curr stack) in
                   match result with
                   | Num num -> Stack.push num stack; eval rest stack
@@ -46,13 +42,13 @@ let rec eval tokens stack : result=
   ;;
 
 let rpn (expression) = 
-  let regex = Str.regexp " +" in               (* regex space separator *)
+  let regex = Str.regexp " +" in               (* Regex space separator *)
   let tokens = Str.split regex expression in   (* Get a list of tokens for an expression *)
   let stack = Stack.create () in               (* Creates a new stack *)
-  let result = (eval tokens stack) in          (* result of rpn *)
+  let result = (eval tokens stack) in          (* Result of rpn *)
 
   match result with
-  | Num num -> print_float (num); print_newline ();
+  | Num num     -> print_float (num); print_newline ();
   | Error error -> print_endline (error); 
   ;;
   
