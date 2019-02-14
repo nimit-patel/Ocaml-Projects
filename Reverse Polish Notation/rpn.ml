b1be643d5ef7a175fb11e0c8d82806536aa96798
nimitp <nimit444@gmail.com>
2019-02-14 11:44:33 -0500

    modified error messages

--- Reverse Polish Notation/rpn.ml
+++ Reverse Polish Notation/rpn.ml
@@ -11,14 +11,17 @@ let calc stack op : result =
     Stack.push (op first second) stack;
     Num (op first second)
   with
-    | Stack.Empty      -> Error "Invalid RPN Expression, Stack is empty!"
+    | Stack.Empty      -> Error "Invalid RPN Expression: not enough operands!" 
     | _                -> Error "Something went wrong!"
   ;;
 
 let get_float_value token stack : result = 
-  let float_value = float_of_string(token) in
-  Stack.push float_value stack;
-  Num float_value
+  try 
+    let float_value = float_of_string(token) in
+    Stack.push float_value stack;
+    Num float_value
+  with
+    | _                 -> Error ("Error: " ^ token ^ " cannot be converted to a float")
   ;;
 
 let parse_token token stack : result =
@@ -46,30 +49,29 @@ let is_float value : bool=
 let rec eval tokens stack : result=
   match tokens with 
   | []       -> if (Stack.length stack) = 1 then Num (Stack.pop stack) 
-                else Error "Invalid RPN expression!"
-  | hd :: tl -> let value = (parse_token hd stack) in
-                let is_type_float = is_float value in
+                else Error "Invalid RPN expression: not enough operators!"
+  | hd :: tl -> let result = (parse_token hd stack) in
+                let is_type_float = is_float result in
                 if is_type_float then eval tl stack 
-                else value
+                else result
   ;;
 
 
 let rpn (expression) = 
-  (* Get a list of tokens for an expression *)
-  let regex = Str.regexp " +" in
-  let tokens = Str.split regex expression in
-  (* Create a new stack *)
-  let stack = Stack.create () in
-  let result = (eval tokens stack) in
+  let regex = Str.regexp " +" in               (* regex space separator *)
+  let tokens = Str.split regex expression in   (* Get a list of tokens for an expression *)
+  let stack = Stack.create () in               (* Creates a new stack *)
+  let result = (eval tokens stack) in          (* result of rpn *)
+
   match result with
-  | Num num -> print_float num; print_newline ();
-  | Error error -> print_string error; print_newline ();
+  | Num num -> print_float (num); print_newline ();
+  | Error error -> print_endline (error); 
   ;;
   
-(* read lines of input *)
-let rec read_lines () =
+
+let rec read_lines () =                         (* read lines of input *)
     try let line = read_line () in
-        rpn (line); 
+        rpn (line);
         read_lines ();
     with
       End_of_file -> () 
