ad100da9e49b3216e30110fe20323b04fc72916e
nimitp <nimit444@gmail.com>
2019-02-14 09:14:58 -0500

    Sum types result added

--- Reverse Polish Notation/rpn.ml
+++ Reverse Polish Notation/rpn.ml
@@ -4,51 +4,53 @@ type result =
   | Error of string 
   ;;
 
-let calc stack op =
+let calc stack op : result =
+  try
     let second = Stack.pop stack in
     let first  = Stack.pop stack in
-    (op first second)
+    Num (op first second)
+  with
+    | Stack.Empty      -> Error "Invalid RPN Expression!"
+    | Division_by_zero -> Error "Division by zero!"
+    | _                -> Error "Something went wrong!"
   ;;
 
-  (*  with
-    | Stack.Empty -> print_string "Invalid RPN Expression!"
-    | Division_by_zero -> print_string "Division by zero!"
-    | _ -> print_string "Something went wrong!" *)
 
-let parse token stack =
+let parse_token token stack : result =
   match token with
-  | "*" -> Stack.push ( calc stack  ( *. ) ) stack
-  | "/" -> Stack.push ( calc stack  ( /. ) ) stack
-  | "+" -> Stack.push ( calc stack  ( +. ) ) stack
-  | "-" -> Stack.push ( calc stack  ( -. ) ) stack
-  | "^" -> Stack.push ( calc stack  ( ** ) ) stack
-  |  _  -> Stack.push ( float_of_string(token) ) stack
+  | "*" -> calc stack ( *. )
+  | "/" -> calc stack ( /. )
+  | "+" -> calc stack ( +. )
+  | "-" -> calc stack ( -. )
+  | "^" -> calc stack ( ** )
+  |  _  -> Num ( float_of_string(token) )
   ;;
+
+let get_result result : string =
+  match result with
+  | Num num -> string_of_float num
+  | Error error -> error
+  ;;
+
+let rec eval tokens stack =
+  match tokens with 
+  | []       -> print_string (get_result (Stack.pop stack;))
+  | hd :: tl -> let val = parse_token hd in
+                Stack.push val stack;
+                match val with
+                | Num num -> eval tl stack
+  ;;
+
 
 let rpn (expression) = 
-
   (* Get a list of tokens for an expression *)
   let regex = Str.regexp " +" in
   let tokens = Str.split regex expression in
   (* Create a new stack *)
   let stack = Stack.create () in
-  (* Visit each token and perform necessary operation *)
-  List.iter (fun token -> parse token stack) tokens;
-  Stack.iter (fun top -> print_float top; print_newline ();) stack
-  ;; 
-
-(* 2. Interprets each line as a RPN with " "(space) as a separator
-
-3. Evaluates each RPN and either produces an error 
-(if the RPN is incorrect) or prints a result if it is correct. 
-The result or the error are printed on a separate line (in order) 
-for each RPN in the input (one line in, one line out)
-
-You only need to implement floating point numbers 
-(no distinction between integer and float), implement the 
-following binary operators: +,-,*,/,^(power, i.e x^3 is x cubed).
- No need to implement any unary operator.*)
-
+  eval tokens stack;
+  ;;
+  
 (* read lines of input *)
 let rec read_lines () =
     try let line = read_line () in
