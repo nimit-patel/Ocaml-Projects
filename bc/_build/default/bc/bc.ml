3aa398d5552456ed55e3ab0f4c597fa66275ba2f
nimit-patel <nimit444@gmail.com>
2019-03-20 22:56:09 -0400

    Added runCode for all code blocks to run

--- bc/_build/default/bc/bc.ml
+++ bc/_build/default/bc/bc.ml
@@ -2,6 +2,11 @@ open Core.Std ;;
 open Caml ;;
 module Scope = Caml.Map.Make(String) ;;
 
+let globalStack = Stack.create () ;;
+let localStack = Stack.create () ;;
+Stack.push Scope.empty localStack ;;
+Stack.push Scope.empty globalStack ;;
+
 type sExpr = 
     | Atom of string
     | List of sExpr list
@@ -31,29 +36,10 @@ type env = float Scope.t ;;
 
 type envQueue = env Stack.t;;
 
-let globalStack = Stack.create () ;;
-let localStack = Stack.create () ;;
-Stack.push Scope.empty localStack ;;
-Stack.push Scope.empty globalStack ;;
+let funcMap : statement list Scope.t ref = ref Scope.empty;;
+let paramMap : string list Scope.t ref = ref Scope.empty;;
 
-(* Gets value from the global scope *)    
-let rec getGlobalValue (var: string) : float =
-    let globalScope = Stack.top globalStack in
-    let value = Scope.find_opt var globalScope in
-    match value with
-    | Some(flt)     -> flt
-    | None          -> 0.0
-    ;;
-
-let varEval (var: string) (scopes :envQueue): float  = 
-    let topScope = Stack.top scopes in
-    let value = Scope.find_opt var topScope in
-    match value with
-    | Some(flt)     -> flt
-    | None          -> getGlobalValue var
-    ;;
-
-
+(* Puts value into the appropriate scope *)
 let assignVar (var: string) (value : float) (scopes :envQueue): unit = 
     let localScope = Stack.pop scopes in
     let globalScope = Stack.pop globalStack in
@@ -73,6 +59,25 @@ let assignVar (var: string) (value : float) (scopes :envQueue): unit =
     end
     ;;
 
+(* Gets value from the global scope *)    
+let rec getGlobalValue (var: string) (scopes :envQueue) : float =
+    let globalScope = Stack.top globalStack in
+    let value = Scope.find_opt var globalScope in
+    match value with
+    | Some(flt)     -> flt
+    | None          -> assignVar var 0.0 scopes; (* puts default value 0 for this new variable *)
+                       0.0
+    ;;
+
+(* Gets value from the local scope *)
+let varEval (var: string) (scopes :envQueue): float  = 
+    let topScope = Stack.top scopes in
+    let value = Scope.find_opt var topScope in
+    match value with
+    | Some(flt)     -> flt
+    | None          -> getGlobalValue var scopes
+    ;;
+
 let evalPre (addVal : float) (exp : expr) (scopes :envQueue) : float =
     match exp with
         | Var(var)  -> let value = varEval var scopes in 
@@ -82,18 +87,15 @@ let evalPre (addVal : float) (exp : expr) (scopes :envQueue) : float =
         | _         -> 0.0 (* TO DO throw error *)
     ;;
 
-
-
 let evalRel (op: string)  (left: float) (right: float) : float =
     let diff = left -. right in
-
     match op with
-    | ">"    -> if diff > 0.0 then 1.0 else 0.0
-    | "<"    -> if diff < 0.0 then 1.0 else 0.0
-    | ">="   -> if diff >= 0.0 then 1.0 else 0.0
-    | "<="   -> if diff <= 0.0 then 1.0 else 0.0
-    | "=="   -> if diff == 0.0 then 1.0 else 0.0
-    | "!="   -> if diff != 0.0 then 1.0 else 0.0
+    | ">"    -> if diff > 0.  then 1.0 else 0.0
+    | "<"    -> if diff < 0.  then 1.0 else 0.0
+    | ">="   -> if diff >= 0. then 1.0 else 0.0
+    | "<="   -> if diff <= 0. then 1.0 else 0.0
+    | "=="   -> if diff = 0. then 1.0 else 0.0
+    | "!="   -> if diff <> 0. then 1.0 else 0.0
     ;;
 
 let evalLogical (op: string)  (left: float) (right: float) : float =
@@ -138,25 +140,33 @@ match op with
     | _   -> 0.0  (* To do throw error *)
     ;;
 
-
 let rec evalStatement (s: statement) (scopes :envQueue): envQueue =
     match s with 
-        | Assign(var, expr)  ->  let value = evalExpr expr scopes in
+        | Assign(var, expr) ->  let value = evalExpr expr scopes in
                                 assignVar var value scopes;
                                 scopes
-        | If(e, codeT, codeF) -> 
-            let cond = evalExpr e scopes in
+        | Expr(expr)        -> let result = evalExpr expr scopes in
+                                result |> printf "%F\n";
+                                scopes
+        | If(exp, codeT, codeF) -> 
+            let cond = evalExpr exp scopes in
                 if(cond > 0.0) then 
                     evalCode codeT scopes 
                 else
                     evalCode codeF scopes
             ;
             scopes
-        | Expr(expr)          -> let result = evalExpr expr scopes in
-                                 result |> printf "%F\n";
-                                 scopes
+        | While(cond, stat_list)      -> while (evalExpr cond scopes) = 1.0 do
+                                            evalCode stat_list scopes 
+                                         done;
+                                         scopes
+        | For(init, cond, update, stat_list) -> let tmp = evalStatement init scopes in
+                                                evalForLoop cond update stat_list scopes;
+                                                scopes
+        | FctDef (name, params, stat_list) -> putFuncDef name params stat_list;
+                                              scopes
         | _ -> scopes (*ignore *)
-
+        ;
 and evalCode (stat_list: block) (scopes :envQueue): unit = 
     (* crate new environment *)
     (* user fold_left  *)
@@ -165,6 +175,33 @@ and evalCode (stat_list: block) (scopes :envQueue): unit =
     | hd::tl        -> let s = evalStatement hd scopes in
                        evalCode tl scopes
     | _             -> ()
+    ;
+and evalForLoop (cond : expr) (update: statement) (stat_list: statement list) (scopes: envQueue): unit = 
+    if (evalExpr cond scopes) <> 1.0 then
+       ()
+    else begin
+        evalCode stat_list scopes;
+        let tmp = evalStatement update scopes in
+        evalForLoop cond update stat_list scopes
+    end
+    ;
+and putFuncDef (name : string) (params : string list) (stat_list : statement list) : unit =
+    (*
+    let funcMap = Stack.pop funcStack in
+    let key = string_of_int (List.length params) ^ name in
+    let impl = Scope.find_opt key funcMap in
+    
+    (*
+    let params = Param.find_opt key !paramMap in
+    *)
+
+    match impl with
+    | list  -> let funcMap = Scope.add key stat_list funcMap in
+               Stack.push funcMap funcStack
+    | None  -> ()
+    
+    *)
+    ()
     ;;
 
 (* Test for expression *)
@@ -179,25 +216,24 @@ let%expect_test "evalNum" =
 (* Test for variable *)
 let%expect_test "evalVar" = 
     let var = Var("i") in
+
     let scope = Scope.empty in
     let global = Scope.empty in
-
     let scope = Scope.add "i" 24.0 scope in
     let global = Scope.add "r" 23.0 global in
-    
     Stack.push scope localStack;
     Stack.push global globalStack;
 
     evalExpr var localStack |>
     printf "%F";
     [%expect {| 24. |}]
-
+    ;;
 (* 
-    v = 10; 
-    v // display v
-    ++v
+    v = 4; 
+    v //  4
+    ++v // 5
     v = v + 4 + v - 4
-    v 
+    v  // 10
  *)
 let p1: block = [
         Assign("v", Num(4.0));
@@ -205,18 +241,68 @@ let p1: block = [
         Expr(Op1("++", Var("v")));
         Assign("v", Op2("+", Op2("+", Var("v"), Num(4.0)),  Op2("-", Var("v"), Num(4.0))));
         Expr(Var("v"));
+        Expr(Op2("==", Var("v"), Num(10.0)));
+        Expr(Op2("!=", Var("v"), Num(10.0)));
 ];;
-
 
 let%expect_test "p1" =
     evalCode p1 localStack; 
     [%expect {| 
                 4.
                 5. 
-                9.
+                10.
+                1.
+                0.
                 |}]
     ;;
 
+(* 
+    If else test
+    v = 0
+
+    if (v - 4) < 0.0 then
+    ++v
+    else
+    --v
+*)
+let ifelse: block = [
+    Assign("v", Num(0.0));
+    If(
+        Op2("<", Op2("-",  Var("v"), Num(4.0)), Num(0.0)), 
+        [Expr(Op1("++", Var("v")))], 
+        [Expr(Op1("--", Var("v")))]
+    );
+];;
+
+let%expect_test "ifelse" =
+    evalCode ifelse localStack; 
+    [%expect {| 
+              1.
+              |}]
+    ;;
+
+let while_test: block = [
+    While(
+        Op2("<", Var("k"), Num(10.0)),
+        [Expr(Op1("++", Var("k")))]
+    );
+];;
+
+let%expect_test "while_test" =
+evalCode while_test localStack; 
+[%expect {| 
+            1.
+            2.
+            3.
+            4.
+            5.
+            6.
+            7.
+            8.
+            9.
+            10.
+            |}]
+;;
 (*
     v = 1.0;
     if (v>10.0) then
@@ -235,22 +321,20 @@ let p2: block = [
         [For(
             Assign("i", Num(2.0)),
             Op2("<", Var("i"), Num(10.0)),
-            Expr(Op1("++a", Var("i"))),
+            Assign("i",(Op2("+", Var("i"), Num(1.0)))),
             [
                 Assign("v", Op2("*", Var("v"), Var("i")))
             ]
-        )]
+        )];
     );
     Expr(Var("v"))
 ];;
 
-(*
-let%expect_test "p1" =
-    evalCode p2 []; 
-    [%expect {| 3628800. |}]
-    ;;
 
-    *)
+let%expect_test "p1" =
+    evalCode p2 localStack; 
+    [%expect {| 362880. |}]
+    ;;
 
 (*  Fibbonaci sequence
     define f(x) {
@@ -290,9 +374,3 @@ let%expect_test "p3" =
 
 (* ADD run. Internal func can change *)
 (* Read no needed *)
-(*
-// var assignment --> 
-// use curr scope if var exist there
-// use global scope if var exist there
-// use local scope create new var 
-*)
